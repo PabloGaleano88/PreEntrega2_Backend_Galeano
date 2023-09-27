15942b0f4790b9bfa7542b389b99930b4e2e00e2
PabloGaleano88 <pablogaleano88@hotmail.com>
2023-09-27 18:18:00 -0300

    vinculacion cart con products en el schema

--- src/dao/models/cartsModel.js
+++ src/dao/models/cartsModel.js
@@ -3,13 +3,17 @@ import mongoose from 'mongoose'
 const cartsCollection = "carts"
 
 const cartsSchema = new mongoose.Schema({
-    products: [{
-        productId: {
-            type: String,
-            unique: true
-        },
-        quantity: Number
-    }]
+    products: {
+        type:[
+            {
+                productId: {
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"products"
+                },
+                quantity: Number
+            }
+        ]
+    }
 })
 
 const cartsModel = mongoose.model(cartsCollection, cartsSchema)
